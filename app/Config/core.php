91c4da2a8d81a66ab700474bbbbaa06f79290a3f
Jesse Tessmer <jesse.tessmer@gmail.com>
2014-05-24 18:27:30 -0500

    gitignore

--- app/Config/core.php
+++ app/Config/core.php
@@ -222,12 +222,12 @@
 /**
  * A random string used in security hashing methods.
  */
-	Configure::write('Security.salt', 'DYhG93b0qyJfIxfs2guVoUubWwvniR2G0FgaC9mi');
+	Configure::write('Security.salt', '097Il!&i$dYpo75?Mx4!iOf?8?&m5f&$BCR$@e!Z#bvECQi&');
 
 /**
  * A random numeric string (digits only) used to encrypt/decrypt strings.
  */
-	Configure::write('Security.cipherSeed', '76859309657453542496749683645');
+	Configure::write('Security.cipherSeed', '377487299698383312619298573197624');
 
 /**
  * Apply timestamps with the last modified time to static assets (js, css, images).
